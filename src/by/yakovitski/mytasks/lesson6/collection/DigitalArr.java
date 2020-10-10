552e93260448dcdbe5693d289b49327d3840278e
max-yakovitski <m.yakovitski@gmail.com>
2020-10-10 23:11:28 +0300

    PoemTest

--- src/by/yakovitski/mytasks/lesson6/collection/DigitalArr.java
+++ src/by/yakovitski/mytasks/lesson6/collection/DigitalArr.java
@@ -4,8 +4,7 @@ import java.util.*;
 
 public class DigitalArr {
     public static void main(String[] args) {
-        List <Integer> digitalList = new ArrayList<>();//можно создать сразу TreeSet
-        // - хранит только уникальные элементы
+        List <Integer> digitalList = new ArrayList<>();
         Random random = new Random();
         for (int i = 0; i < 10; i++) {
             digitalList.add(random.nextInt(10));
@@ -13,7 +12,7 @@ public class DigitalArr {
         System.out.println(digitalList);
         Collections.sort(digitalList);
         for (int i = 0; i < digitalList.size() - 1; i++) {
-            if (digitalList.get(i) == digitalList.get(i + 1)) {
+            if (digitalList.get(i).equals(digitalList.get(i + 1))) {
                 digitalList.remove(digitalList.get(i + 1));
                 i--;
             }
---
+++ src/by/yakovitski/mytasks/lesson6/poem/PoemTest.java
@@ -0,0 +1,18 @@
+package by.yakovitski.mytasks.lesson6.poem;
+
+import java.util.ArrayList;
+
+public class PoemTest {
+    public static void main(String[] args) {
+        Poem poem = new Poem(new ArrayList<String>());
+        poem.addText("А хто там ідзе, а хто там ідзе");
+        poem.addText("У агромністай такой грамадзе?");
+        poem.addText("— Беларусы.");
+        poem.addText("А што яны нясуць на худых плячах,");
+        poem.addText("На руках ў крыві, на нагах у лапцях?");
+        poem.addText("— Сваю крыўду.");
+        poem.print();
+        poem.sortLine();
+        poem.print();
+    }
+}
