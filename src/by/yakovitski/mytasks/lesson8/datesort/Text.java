44961fc6523ecfe928ebc4858311b247e89e7db8
max-yakovitski <m.yakovitski@gmail.com>
2020-11-08 00:47:53 +0300

    DateSort 1.0

---
+++ src/by/yakovitski/mytasks/lesson8/datesort/Text.java
@@ -0,0 +1,33 @@
+package by.yakovitski.mytasks.lesson8.datesort;
+
+import java.io.*;
+
+public class Text {
+    public static void main(String[] args) throws IOException {
+        File file = new File("/Users/ma_yak/HomeWork/src/by/yakovitski/mytasks/lesson8/datesort/People");
+        makeDir(file);
+    }
+    public static void makeDir(File file) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        String line;
+
+        while((line = reader.readLine()) != null) {
+            String [] text = line.split(" - ");
+            File fileDir = new File("/Users/ma_yak/HomeWork/src/by/yakovitski/mytasks/lesson8/datesort", text[0]);
+            if (!fileDir.exists()){
+                File fileInfo = new File("/Users/ma_yak/HomeWork/src/by/yakovitski/mytasks/lesson8/datesort/" + text[0], "info.txt");
+                fileDir.mkdir();
+                BufferedWriter writer = new BufferedWriter(new FileWriter(fileInfo));
+                writer.write(text[1]);
+                writer.close();
+            }
+            else {
+                FileWriter fileWriter = new FileWriter("/Users/ma_yak/HomeWork/src/by/yakovitski/mytasks/lesson8/datesort/" + text[0] + "/info.txt", true);
+                BufferedWriter writer1 = new BufferedWriter(fileWriter);
+                writer1.write("\n" + text[1]);
+                writer1.close();
+            }
+        }
+        reader.close();
+    }
+}
